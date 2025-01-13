eac66fa0a794965724a809d10dac0630953f2e13
Raluca <spatarularisaraluca@gmail.com>
2025-01-13 02:23:00 +0200

    Deploy

--- 01-starting-project/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { AppComponent } from './app/app.component';
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
-
-bootstrapApplication(AppComponent, {
-    providers: [provideExperimentalZonelessChangeDetection()],
-}).catch((err) => console.error(err));
